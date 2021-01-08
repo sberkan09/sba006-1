b149e2e3a465807498cee1fbe3cc46df2bde98d7
sberkan09 <samiakkaya396@gmail.com>
2021-01-08 19:51:34 +0300

    Update db.php

--- db.php
+++ db.php
@@ -5,6 +5,6 @@ mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem",
 mysqli_real_connect($conn, "sba006-1.mysql.database.azure.com", "sberkan09@sba006-1", "EtuSami006", "test", 3306, MYSQLI_CLIENT_SSL);*/
 
 $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem'
+    PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/DigiCertGlobalRootCA.crt.pem'
 );
 $db = new PDO('mysql:host=sba006-1.mysql.database.azure.com;port=3306;dbname=test', 'sberkan09', 'EtuSami006', $options);
